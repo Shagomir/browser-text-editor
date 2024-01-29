033c91cf4f18f5d1972c9e58611cffcac209f2e3
Shagomir <m.j.knudson@gmail.com>
2024-01-29 15:34:20 -0600

    starter code + updates to add missing features

---
+++ client/src-sw.js
@@ -0,0 +1,63 @@
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute, Route } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: "page-cache",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ["/index.html", "/"],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
+
+// TODO: Implement asset caching
+// Handle images:
+const imageRoute = new Route(
+  ({ request }) => {
+    return request.destination === "image";
+  },
+  new StaleWhileRevalidate({
+    cacheName: "images",
+  })
+);
+
+// Handle scripts:
+const scriptsRoute = new Route(
+  ({ request }) => {
+    return request.destination === "script";
+  },
+  new CacheFirst({
+    cacheName: "scripts",
+  })
+);
+
+// Handle styles:
+const stylesRoute = new Route(
+  ({ request }) => {
+    return request.destination === "style";
+  },
+  new CacheFirst({
+    cacheName: "styles",
+  })
+);
+
+// Register routes
+registerRoute(imageRoute);
+registerRoute(scriptsRoute);
+registerRoute(stylesRoute);
---
+++ client/webpack.config.js
@@ -0,0 +1,67 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "Webpack Plugin",
+    }),
+    new MiniCssExtractPlugin(),
+    new InjectManifest({
+      swSrc: "./src/sw.js",
+      swDest: "service-worker.js",
+    }),
+    new WebpackPwaManifest({
+      name: "TODOs",
+      short_name: "TODOs",
+      description: "Keep track of important tasks!",
+      background_color: "#7eb4e2",
+      theme_color: "#7eb4e2",
+      start_url: "./",
+      publicPath: "./",
+      icons: [
+        {
+          src: path.resolve("assets/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
